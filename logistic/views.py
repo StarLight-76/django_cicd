50e699788987099eef2d0c7ac02ac01dc9f21c0c
StarLight-76 <dmitry.kirilchuk@hotmail.com>
2026-09-18 19:37:45 +0300

    Change view 2

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Ура, получилось!'})
+    return Response({'message': 'Finish dev CI wint deploy!!!'})
